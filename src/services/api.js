e832b8fd96d75d13c446d3f711ceb65499c6ea14
maryori <mary.gutierrez@duocuc.cl>
2026-02-03 21:55:58 -0300

    api

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,6 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL
 });
 
-// Agregar token automáticamente
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
